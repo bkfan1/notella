87521034e35339d4ab09a364caa4def6007f2fc2
bkfan1 <blossomsc15@gmail.com>
2022-08-10 19:00:22 -0400

    Removed unused imports from context consuming + improved some ternary conditionals in buttons tags

--- components/MainActionsMenu.js
+++ components/MainActionsMenu.js
@@ -1,37 +1,82 @@
-import {useContext} from "react";
+import { useContext } from "react";
 import { NotesContext } from "../context/NotesContext";
 import { LayoutContext } from "../context/LayoutContext";
 import axios from "axios";
 import { useRouter } from "next/router";
 
-export default function MainActionsMenu(){
-    const {addNewNote, viewTrashedNotes, setViewTrashedNotes} = useContext(NotesContext);
-    const {darkMode, handleClickChangeDarkMode} = useContext(LayoutContext);
+export default function MainActionsMenu() {
+  const { addNewNote, viewTrashedNotes, setViewTrashedNotes} =
+    useContext(NotesContext);
+  const {
+    darkMode,
+    handleClickChangeDarkMode,
+  } = useContext(LayoutContext);
 
-    const router = useRouter();
+  const router = useRouter();
 
-    const btnTheme = darkMode ? 'actionBtn__dark' : '';
-    
-    return(
-        <>
-        <menu className={`mainActionsMenu flex gap-4 justify-center items-center w-64 h-12 px-4 border-b ${darkMode ? 'border-gray-700' : 'border-gray-300'}`}>
-            <button onClick={addNewNote} className={`${btnTheme}`}>
-                <i className="bi bi-journal-plus text-2xl"/>
-            </button>
+  const btnTheme = darkMode ? "actionBtn__dark" : "";
 
-            <button onClick={()=>setViewTrashedNotes(!viewTrashedNotes)} className={`${btnTheme}`} title={"Trashed notes"}>
-                <i className={`bi bi-trash3${viewTrashedNotes ? '-fill' : ''} text-2xl`}/>
-            </button>
+  const logout = async () => {
+    const res = await axios.delete("/api/auth/logout");
+    res.status === 200
+      ? router.push("/login")
+      : console.warn("something went wrong");
+  };
 
-            <button onClick={handleClickChangeDarkMode} className={`${btnTheme}`} title={darkMode ? 'Switch to light mode' : 'Switch to dark mode'}>
-                <i className={`${darkMode ? 'bi bi-sun-fill' : 'bi bi-moon'} text-2xl`}/>
-            </button>
+  return (
+    <>
+      <menu
+        className={`mainActionsMenu flex gap-4 justify-center items-center w-full h-12 px-4 py-2 border-b ${
+          darkMode ? "bg-gray-800 border-gray-700" : "bg-white border-gray-300"
+        }`}
+      >
+        <button onClick={logout} className={`${btnTheme}`} title={"Log out"}>
+          <i className="bi bi-box-arrow-left text-2xl" />
+        </button>
 
-            <button onClick={()=>router.push('/settings')} className={`${btnTheme}`} >
-                <i className="bi bi-gear text-2xl"/>
-            </button>
-            
-        </menu>
-        </>
-    )
+        {viewTrashedNotes ? (
+          ""
+        ) : (
+          <button
+            onClick={addNewNote}
+            className={`${btnTheme}`}
+            title={"Add new note"}
+          >
+            <i className="bi bi-journal-plus text-2xl" />
+          </button>
+        )}
+
+        <button
+          onClick={() => setViewTrashedNotes(!viewTrashedNotes)}
+          className={`${btnTheme}`}
+          title={"Trashed notes"}
+        >
+          <i
+            className={`bi bi-trash3${
+              viewTrashedNotes ? "-fill" : ""
+            } text-2xl`}
+          />
+        </button>
+
+        <button
+          onClick={handleClickChangeDarkMode}
+          className={`${btnTheme}`}
+          title={darkMode ? "Switch to light mode" : "Switch to dark mode"}
+        >
+          <i
+            className={`${darkMode ? "bi bi-sun-fill" : "bi bi-moon"} text-2xl`}
+          />
+        </button>
+
+        <button
+          onClick={() => router.push("/settings")}
+          className={`${btnTheme}`}
+          title={'Settings'}
+        >
+          <i className="bi bi-gear text-2xl" />
+        </button>
+
+      </menu>
+    </>
+  );
 }
