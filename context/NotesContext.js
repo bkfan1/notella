10c788ef6a0e47581e85f8e34296cd025320487f
bkfan1 <blossomsc15@gmail.com>
2022-08-10 19:07:58 -0400

    Added new useEffects, state variables and consumed many values from layout context

--- context/NotesContext.js
+++ context/NotesContext.js
@@ -1,156 +1,202 @@
-import {createContext, useEffect, useContext, useState} from "react";
+import { createContext, useEffect, useContext, useState } from "react";
 import { LayoutContext } from "./LayoutContext";
 import axios from "axios";
 import { nanoid } from "nanoid";
 
-
 export const NotesContext = createContext();
 
+export const NotesProvider = ({ children, notes, trashedNotes }) => {
+  const { focusMode, setFocusMode, panelIsActive, setPanelIsActive, windowWidth } = useContext(LayoutContext);
 
-export const NotesProvider = ({children, notes, trashedNotes})=>{
+  const [userNotes, setUserNotes] = useState(notes);
+  const [userTrashedNotes, setUserTrashedNotes] = useState(trashedNotes);
 
-    const {focusMode,setFocusMode} = useContext(LayoutContext);
+  const [searchValue, setSearchValue] = useState("");
+  const [filteredNotes, setFilteredNotes] = useState([]);
 
-    const [userNotes, setUserNotes] = useState(notes);
-    const [userTrashedNotes, setUserTrashedNotes] = useState(trashedNotes);
+  const [currentEditingNote, setCurrentEditingNote] = useState(null);
+  const [viewTrashedNotes, setViewTrashedNotes] = useState(false);
 
-    const [currentEditingNote, setCurrentEditingNote] = useState(null);
-
-    const [viewTrashedNotes, setViewTrashedNotes] = useState(false);
-
-    useEffect(()=>{
-
-        const updateNotes = async ()=>{
-            const data = {notes: userNotes, trashedNotes: userTrashedNotes}
-            const res = await axios.post('/api/notes',data);
-
-            res.status === 200 ? console.log("notas actualizadas exitosamente") : console.log("ha ocurrido un error al intentar actualizar las notas")
+  useEffect(() => {
+    const filterNotesBySearchTerm = () => {
+      if (searchValue === "") {
+        if (viewTrashedNotes) {
+          setFilteredNotes(userTrashedNotes);
+          return;
+        } else {
+          setFilteredNotes(userNotes);
+          return;
         }
+      }
 
-        const timer = setTimeout(()=>{
-            updateNotes();
-        }, 8000);
+      let results;
+      if (viewTrashedNotes) {
+        results = [...userTrashedNotes];
+      } else {
+        results = [...userNotes];
+      }
 
+      setFilteredNotes(
+        results.filter(
+          (note) =>
+            note.title === searchValue || note.title.includes(searchValue)
+        )
+      );
+    };
 
-        return ()=> clearInterval(timer);
+    filterNotesBySearchTerm();
 
+  }, [searchValue, viewTrashedNotes, userNotes, userTrashedNotes]);
 
+  useEffect(() => {
+    const updateNotes = async () => {
+      const data = { notes: userNotes, trashedNotes: userTrashedNotes };
+      const res = await axios.put("/api/account/notes", data);
 
+      res.status === 200
+        ? console.log("notas actualizadas exitosamente")
+        : console.log("ha ocurrido un error al intentar actualizar las notas");
+    };
 
-    }, [userNotes, userTrashedNotes])
+    const timer = setTimeout(() => {
+      updateNotes();
+    }, 8000);
 
-    useEffect(()=>{
+    return () => clearInterval(timer);
+  }, [userNotes, userTrashedNotes]);
 
-        setCurrentEditingNote(null);
+  useEffect(() => {
+    setCurrentEditingNote(null);
+  }, [viewTrashedNotes]);
 
-
-    }, [viewTrashedNotes])
-
-    useEffect(()=>{
-        if(userTrashedNotes.length === 0){
-            setViewTrashedNotes(false);
-        }
-
-    }, [userTrashedNotes])
-
-    const addNewNote = ()=>{
-        if(viewTrashedNotes){setViewTrashedNotes(false);}
-
-        const newNote = {
-            id: nanoid(),
-            title: 'Untitled note',
-            body: "Content goes here"
-        }
-
-        setUserNotes([...userNotes, newNote]);
+  const addNewNote = () => {
+    if (viewTrashedNotes) {
+      setViewTrashedNotes(false);
     }
 
-    const deleteNote = (id)=>{
+    const newNote = {
+      id: nanoid(),
+      title: "Untitled note",
+      body: "Content goes here",
+    };
 
-        const index = userNotes.findIndex((note)=>note.id === id);
-        const newUserNotes = [...userNotes];
-        newUserNotes.splice(index, 1);
+    // Append new note at the beginning
+    setUserNotes([newNote, ...userNotes]);
+  };
 
-        setUserNotes(newUserNotes);
+  const deleteNote = (id) => {
+    const index = userNotes.findIndex((note) => note.id === id);
+    const newUserNotes = [...userNotes];
+    newUserNotes.splice(index, 1);
 
-    }
+    setUserNotes(newUserNotes);
+  };
 
-    const handleClickNoteInRecipient = (id)=>{
-        let clickedNote;
+  const handleClickNoteInRecipient = (id) => {
+    
+    let clickedNote;
 
-        if(viewTrashedNotes){
-            clickedNote = userTrashedNotes.find((note)=>note.id === id);
-            setCurrentEditingNote(clickedNote);
-            return;
-        }
-        clickedNote = userNotes.find((note)=>note.id === id);
-        setCurrentEditingNote(clickedNote);
-        return;
-    }
-
-    const handleOnChangeCurrentEditingNote = (e, id)=>{
-        const {target} = e;
-        const {name, value} = target;
-
-        const index = userNotes.findIndex((note)=>note.id === id);
-        let copiedNote = Object.assign({}, userNotes[index]);
-        copiedNote[name] = value;
-
-        const copiedNotes = [...userNotes];
-        copiedNotes.splice(index, 1, copiedNote);
-
-        setCurrentEditingNote(copiedNote);
-        setUserNotes(copiedNotes);
-
-    }
-
-    const handleDeleteCurrentEditingNote = ()=>{
-
-        const copiedCurrentEditingNote = {...currentEditingNote};
-        setCurrentEditingNote(null);
-
-        if(focusMode){setFocusMode(false);}
-
-        const index = userNotes.findIndex((note)=>note.id === copiedCurrentEditingNote.id);
-
-        const updatedUserNotes = [...userNotes];
-        const [removed] = updatedUserNotes.splice(index, 1);
-
-        const newTrashedNotes = [...userTrashedNotes]
-        newTrashedNotes.push(removed);
-
-        setUserNotes(updatedUserNotes);
-        setUserTrashedNotes(newTrashedNotes);
-    }
-
-    const handleRemoveNoteFromTrash = (id)=>{
-
-        setCurrentEditingNote(null);
-        const index = userTrashedNotes.findIndex((note)=>note.id === id);
-
-        const newTrashedNotes = [...userTrashedNotes];
-        const [removed] = newTrashedNotes.splice(index, 1);
-
-        setUserTrashedNotes(newTrashedNotes);
-        setUserNotes([...userNotes, removed]);
-    }
-
-    const handleDeleteNoteFromTrash = ()=>{
-        const copiedCurrentEditingNote = {...currentEditingNote};
-        setCurrentEditingNote(null);
-        const index = userTrashedNotes.findIndex((note)=>note.id === copiedCurrentEditingNote.id);
-        const updatedTrashedNotes = [...userTrashedNotes];
-        updatedTrashedNotes.splice(index, 1);
-        setUserTrashedNotes(updatedTrashedNotes);
+    if(windowWidth < 1024){
+      if(panelIsActive){
+        setPanelIsActive(false);
+      }
     }
 
 
-    return(
-        <>
-        <NotesContext.Provider value={{userNotes, userTrashedNotes, addNewNote, deleteNote, viewTrashedNotes, setViewTrashedNotes, handleClickNoteInRecipient, currentEditingNote, handleOnChangeCurrentEditingNote, handleDeleteCurrentEditingNote, handleRemoveNoteFromTrash, handleDeleteNoteFromTrash}}>
-            {children}
-        </NotesContext.Provider>
-        </>
-    )
+    if (viewTrashedNotes) {
+      clickedNote = userTrashedNotes.find((note) => note.id === id);
+    } else {
+      clickedNote = userNotes.find((note) => note.id === id);
+    }
 
-}
+    setCurrentEditingNote(clickedNote);
+
+    
+    
+  };
+
+  const handleOnChangeCurrentEditingNote = (e, id) => {
+    const { target } = e;
+    const { name, value } = target;
+
+    const index = userNotes.findIndex((note) => note.id === id);
+    let copiedNote = Object.assign({}, userNotes[index]);
+    copiedNote[name] = value;
+
+    const copiedNotes = [...userNotes];
+    copiedNotes.splice(index, 1, copiedNote);
+
+    setCurrentEditingNote(copiedNote);
+    setUserNotes(copiedNotes);
+  };
+
+  const handleDeleteCurrentEditingNote = () => {
+    const copiedCurrentEditingNote = { ...currentEditingNote };
+    setCurrentEditingNote(null);
+
+    if (focusMode) {
+      setFocusMode(false);
+    }
+
+    const index = userNotes.findIndex(
+      (note) => note.id === copiedCurrentEditingNote.id
+    );
+
+    const updatedUserNotes = [...userNotes];
+    const [removed] = updatedUserNotes.splice(index, 1);
+
+    const newTrashedNotes = [...userTrashedNotes];
+    newTrashedNotes.push(removed);
+
+    setUserNotes(updatedUserNotes);
+    setUserTrashedNotes(newTrashedNotes);
+  };
+
+  const handleRemoveNoteFromTrash = (id) => {
+    setCurrentEditingNote(null);
+    const index = userTrashedNotes.findIndex((note) => note.id === id);
+
+    const newTrashedNotes = [...userTrashedNotes];
+    const [removed] = newTrashedNotes.splice(index, 1);
+
+    setUserTrashedNotes(newTrashedNotes);
+    setUserNotes([...userNotes, removed]);
+  };
+
+  const handleDeleteNoteFromTrash = () => {
+    const copiedCurrentEditingNote = { ...currentEditingNote };
+    setCurrentEditingNote(null);
+    const index = userTrashedNotes.findIndex(
+      (note) => note.id === copiedCurrentEditingNote.id
+    );
+    const updatedTrashedNotes = [...userTrashedNotes];
+    updatedTrashedNotes.splice(index, 1);
+    setUserTrashedNotes(updatedTrashedNotes);
+  };
+
+  return (
+    <>
+      <NotesContext.Provider
+        value={{
+          userNotes,
+          userTrashedNotes,
+          addNewNote,
+          deleteNote,
+          viewTrashedNotes,
+          setViewTrashedNotes,
+          handleClickNoteInRecipient,
+          currentEditingNote,
+          handleOnChangeCurrentEditingNote,
+          handleDeleteCurrentEditingNote,
+          handleRemoveNoteFromTrash,
+          handleDeleteNoteFromTrash,
+          searchValue,
+          setSearchValue,
+          filteredNotes,
+        }}
+      >
+        {children}
+      </NotesContext.Provider>
+    </>
+  );
+};
