c13487de363e7183a726a9589859db19700760b6
bkfan1 <blossomsc15@gmail.com>
2022-08-10 19:03:40 -0400

    Added folder to organize project api endpoints structure

---
+++ pages/api/auth/login.js
@@ -0,0 +1,14 @@
+import { handleLogin } from "../../../database/middleware/authentication/login/index";
+
+export default async function handler(req, res) {
+  switch (req.method) {
+    case "POST":
+      return await handleLogin(req, res);
+
+      break;
+
+    default:
+      res.status(403).json({});
+      break;
+  }
+}
---
+++ pages/api/auth/logout.js
@@ -0,0 +1,15 @@
+import { verifyToken } from "../../../database/middleware/authentication/token";
+import { handleLogout } from "../../../database/middleware/authentication/logout";
+
+export default async function handler(req, res) {
+  switch (req.method) {
+    case "DELETE":
+      return await verifyToken(req, res, handleLogout);
+
+      break;
+
+    default:
+      res.status(400).json({});
+      break;
+  }
+}
