b26b420e079010993b3310bbb74cca4faa0259c4
bkfan1 <blossomsc15@gmail.com>
2022-08-10 19:06:16 -0400

    Replaced userNotes and userTrashedNotes mapping functions for filteredNotes mapping function

--- components/Recipient.js
+++ components/Recipient.js
@@ -4,59 +4,39 @@ import { LayoutContext } from "../context/LayoutContext";
 import NoteInRecipient from "./NoteInRecipient";
 
 export default function Recipient() {
-  const { userNotes, userTrashedNotes, viewTrashedNotes } =
+  const { viewTrashedNotes, setSearchValue, filteredNotes } =
     useContext(NotesContext);
-    const {darkMode} = useContext(LayoutContext);
+  const { darkMode } = useContext(LayoutContext);
 
-    
   return (
     <>
-      <aside className={`recipient border-b ${darkMode ? 'border-gray-700' : 'border-gray-300'}`}>
-        <header className={`py-2 border-b ${darkMode ? 'border-gray-700' : 'border-gray-300'} text-center`}>
-          <h1 className={`${darkMode ? 'text-white' : ''} font-bold`}>{viewTrashedNotes ? "Trashed Notes" : "All Notes"}</h1>
-          <input type="text" placeholder="Search by title..." className={`mt-1 px-2 border ${darkMode ? 'border-gray-700 text-white' : 'border-gray-300'} rounded`} style={{background:'none'}}/>
+      <aside
+        className={`recipient overflow-y-scroll border-b ${
+          darkMode ? "bg-gray-800 border-gray-700 " : "bg-white border-gray-300"
+        } h-full`}
+      >
+        <header
+          className={`p-3 border-b ${
+            darkMode ? "border-gray-700" : "border-gray-300"
+          } text-center`}
+        >
+          <h1 className={`${darkMode ? "text-white" : ""} font-bold`}>
+            {viewTrashedNotes ? "Trashed Notes" : "All Notes"}
+          </h1>
+          <input
+            onChange={(e) => setSearchValue(e.target.value)}
+            type="text"
+            placeholder="Search by title..."
+            className={`mt-1 px-2 border ${
+              darkMode ? "border-gray-700 text-white" : "border-gray-300"
+            } rounded`}
+            style={{ background: "none" }}
+          />
         </header>
-        {viewTrashedNotes ? (
-          <>
-            {userTrashedNotes.length === 0 ? (
-              <figure className="flex flex-col items-center justify-center py-2">
-                <h1 className="text-xl font-bold">Nothing here</h1>
-                <p className="text-sm text-center text-gray-500">
-                  It looks like your trash is empty...
-                </p>
-              </figure>
-            ) : (
-              userTrashedNotes.map((trashedNote) => (
-                <NoteInRecipient
-                  key={trashedNote.id}
-                  id={trashedNote.id}
-                  title={trashedNote.title}
-                />
-              ))
-            )}
-          </>
-        ) : (
-          <>
-            {userNotes.length === 0 ? (
-              <>
-                <figure className="flex flex-col items-center justify-center py-2">
-                  <h1 className="text-xl font-bold">Nothing here</h1>
-                  <p className="text-sm text-center text-gray-500">
-                    There is no notes yet
-                  </p>{" "}
-                </figure>
-              </>
-            ) : (
-              userNotes.map((note) => (
-                <NoteInRecipient
-                  key={note.id}
-                  id={note.id}
-                  title={note.title}
-                />
-              ))
-            )}
-          </>
-        )}
+
+        {filteredNotes.map((note) => (
+          <NoteInRecipient key={note.id} id={note.id} title={note.title} />
+        ))}
       </aside>
     </>
   );
