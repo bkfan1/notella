0136cf139f00a2f27f9b7fe1c66f36beb23f3957
bkfan1 <blossomsc15@gmail.com>
2022-08-10 19:11:46 -0400

    Improved middlewares related to register account process

--- database/middleware/register/index.js
+++ database/middleware/register/index.js
@@ -1,20 +1,25 @@
 import connection from "../../connection";
 import { hash } from "bcrypt";
 import Account from "../../models/account";
+import {email} from "../../../utils/regex";
 
 export const registerAccount = async (req, res) => {
   const { body } = req;
 
+  if(!body.email || !body.password){return res.status(403).json({message:'No credentials have been sent.'});}
+
+  if(!email.test(body.email) || body.password.length < 8){return res.status(403).json({message: 'Invalid email or password.'})};
+
   const db = await connection();
 
-  const userWithThatEmail = await Account.find({ email: body.email });
+  const userWithThatEmail = await Account.findOne({ email: body.email });
   if (userWithThatEmail) {
-    return res.status(400).json({ message: "email already registered" });
+    return res.status(400).json({ message: 'Account with that email already exists.' });
   }
 
   hash(body.password, 10, async (err, hashedPassword) => {
     if (err) {
-      return res.status(500).json({});
+      return res.status(500).json({message:'Server error, please try again.'});
     }
 
     const newUser = await Account.create({
@@ -26,6 +31,6 @@ export const registerAccount = async (req, res) => {
       trashedNotes: [],
     });
 
-    return res.status(200).json({ message: "user registered" });
+    return res.status(200).json({ message: 'Account registered successfully.' });
   });
 };
