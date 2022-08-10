e2f4d760dd2d6d2ab312efc85bbad17eacc7ddfa
bkfan1 <blossomsc15@gmail.com>
2022-08-10 19:02:56 -0400

    Removed validCredentials middleware

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,11 +1,10 @@
 import { registerAccount } from "../../database/middleware/register";
-import { validCredentials } from "../../database/middleware/credentials.js";
 
 export default async function handler(req, res) {
   switch (req.method) {
     case "POST":
       return await validCredentials(req, res, registerAccount);
-     
+
       break;
 
     default:
