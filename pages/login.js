3b144b4156e987664b92a606ce79a20ecd5addea
bkfan1 <blossomsc15@gmail.com>
2022-08-10 22:10:44 -0400

    Consumed ResponseStatus Context + used responseStatusBox component

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,17 @@
+import { useContext } from "react";
 import LoginForm from "../components/forms/LoginForm";
+import ResponseStatusBox from "../components/ResponseStatusBox";
+import { ResponseContext } from "../context/ResponseContext";
 
 export default function LoginPage() {
+  const {code, message} = useContext(ResponseContext);
   return (
     <>
       <main className={`loginPage`}>
         <div className="flex flex-col gap-4">
           <h1 className="text-2xl font-bold text-center">Log In</h1>
           <LoginForm />
+          {code && message ? <ResponseStatusBox code={code} message={message}/> : ''}
         </div>
       </main>
     </>
