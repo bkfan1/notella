8886485eb006785e7798ffca0f64085182fabbf9
bkfan1 <blossomsc15@gmail.com>
2022-08-10 22:10:38 -0400

    Consumed ResponseStatus Context + used responseStatusBox component

--- pages/register.js
+++ pages/register.js
@@ -1,13 +1,19 @@
+import { useContext } from "react";
 import RegisterForm from "../components/forms/RegisterForm";
+import ResponseStatusBox from "../components/ResponseStatusBox";
+import { ResponseContext } from "../context/ResponseContext";
 
 export default function RegisterPage() {
+  const {code, message} = useContext(ResponseContext);
   return (
     <>
       <main className="registerPage">
         <div className="flex flex-col gap-4">
           <h1 className="text-2xl font-bold text-center">Register</h1>
           <RegisterForm />
+          {code && message ? <ResponseStatusBox code={code} message={message} /> : ""}
         </div>
+
       </main>
     </>
   );
