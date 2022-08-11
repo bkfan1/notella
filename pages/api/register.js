024981a1b5d072596de9231f1e3238f8c21fab3f
bkfan1 <blossomsc15@gmail.com>
2022-08-10 22:11:46 -0400

    Removed invocation of unused middleware validCredentials

--- pages/api/register.js
+++ pages/api/register.js
@@ -3,7 +3,7 @@ import { registerAccount } from "../../database/middleware/register";
 export default async function handler(req, res) {
   switch (req.method) {
     case "POST":
-      return await validCredentials(req, res, registerAccount);
+      return await registerAccount(req, res);
 
       break;
 
