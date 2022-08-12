8eead55f02c78061b13a05a35ece1e57b8ef1e6e
bkfan1 <blossomsc15@gmail.com>
2022-08-11 21:56:42 -0400

    Added background color based on theme mode

--- pages/login.js
+++ pages/login.js
@@ -1,15 +1,17 @@
 import { useContext } from "react";
 import LoginForm from "../components/forms/LoginForm";
 import ResponseStatusBox from "../components/ResponseStatusBox";
+import { LayoutContext } from "../context/LayoutContext";
 import { ResponseContext } from "../context/ResponseContext";
 
 export default function LoginPage() {
   const {code, message} = useContext(ResponseContext);
+  const {darkMode} = useContext(LayoutContext);
   return (
     <>
-      <main className={`loginPage`}>
+      <main className={`loginPage ${darkMode ? 'bg-gray-800' : 'bg-white'}`}>
         <div className="flex flex-col gap-4">
-          <h1 className="text-2xl font-bold text-center">Log In</h1>
+          <h1 className={`text-2xl font-bold text-center ${darkMode ? 'text-white ': 'text-blue-500'}`}>Log In</h1>
           <LoginForm />
           {code && message ? <ResponseStatusBox code={code} message={message}/> : ''}
         </div>
--- pages/register.js
+++ pages/register.js
@@ -2,14 +2,17 @@ import { useContext } from "react";
 import RegisterForm from "../components/forms/RegisterForm";
 import ResponseStatusBox from "../components/ResponseStatusBox";
 import { ResponseContext } from "../context/ResponseContext";
+import { LayoutContext } from "../context/LayoutContext";
+
 
 export default function RegisterPage() {
   const {code, message} = useContext(ResponseContext);
+  const {darkMode} = useContext(LayoutContext);
   return (
     <>
-      <main className="registerPage">
+      <main className={`registerPage ${darkMode ? 'bg-gray-800' : 'bg-white'}`}>
         <div className="flex flex-col gap-4">
-          <h1 className="text-2xl font-bold text-center">Register</h1>
+          <h1 className={`text-2xl font-bold text-center ${darkMode ? 'text-white ': 'text-blue-500'}`}>Register</h1>
           <RegisterForm />
           {code && message ? <ResponseStatusBox code={code} message={message} /> : ""}
         </div>
