97da20a8cacbaffe4501d2340b176194ee1c2233
bkfan1 <blossomsc15@gmail.com>
2022-08-16 15:52:49 -0400

    added try catch block while attempting to connect to database

--- database/connection.js
+++ database/connection.js
@@ -1,5 +1,9 @@
 import { connect } from "mongoose";
 
-export default async function connection(){
-    return connect(process.env.MONGODB_URI);
+export default async function connection() {
+  try {
+    return await connect(process.env.MONGODB_URI);
+  } catch (error) {
+    throw Error("Error while attempting to connect to database.");
+  }
 }
